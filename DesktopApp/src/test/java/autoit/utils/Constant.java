cd4d85a3574376ee3d8618e218582cebe40d60c5
diana <gilfanova.dianka@mail.ru>
2022-12-02 19:43:28 +0300

    [hw-7] fix constants

--- DesktopApp/src/test/java/autoit/utils/Constant.java
+++ DesktopApp/src/test/java/autoit/utils/Constant.java
@@ -4,8 +4,10 @@ package autoit.utils;
  * @author d.gilfanova
  */
 public class Constant {
+    //path to your notepad.exe
     public static final String APP_PATH = "C:\\Windows\\System32\\notepad.exe";
 
+    //path to your downloaded winium driver
     public static final String WINIUM_DRIVER_PATH = "C:\\MyProgramFiles\\Winium.Desktop.Driver.exe";
 
     public static final String TEST_TEXT = "hello";
